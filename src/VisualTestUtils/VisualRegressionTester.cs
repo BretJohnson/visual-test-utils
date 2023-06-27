ca107b644fff81e7af30da4e7f927015f7d0a058
Bret Johnson <bret.johnson@microsoft.com>
2023-06-27 12:53:54 -0400

    Improve message formatting

--- src/VisualTestUtils/VisualRegressionTester.cs
+++ src/VisualTestUtils/VisualRegressionTester.cs
@@ -5,50 +5,64 @@
         private readonly string snapshotsBaselineDirectory;
         private readonly string snapshotsDiffDirectory;
         private readonly IVisualComparer visualComparer;
-        private readonly double differenceThreshold;
+        private readonly double percentDifferenceThreshold;
         private readonly IVisualDiffGenerator visualDiffGenerator;
 
-        public VisualRegressionTester(string testProjectRootDirectory, IVisualComparer visualComparer, IVisualDiffGenerator visualDiffGenerator, double differenceThreshold = 99.0, string? ciArtifactsDirectory = null)
+        public VisualRegressionTester(string testRootDirectory, IVisualComparer visualComparer, IVisualDiffGenerator visualDiffGenerator, double percentDifferenceThreshold = 1.0, string? ciArtifactsDirectory = null)
         {
-            this.snapshotsBaselineDirectory = Path.Combine(testProjectRootDirectory, "snapshots-baseline");
-            this.snapshotsDiffDirectory = Path.Combine(ciArtifactsDirectory ?? testProjectRootDirectory, "snapshots-diff");
+            this.snapshotsBaselineDirectory = Path.Combine(testRootDirectory, "snapshots-baseline");
+            this.snapshotsDiffDirectory = Path.Combine(ciArtifactsDirectory ?? testRootDirectory, "snapshots-diff");
 
             this.visualComparer = visualComparer;
             this.visualDiffGenerator = visualDiffGenerator;
-            this.differenceThreshold = differenceThreshold;
+            this.percentDifferenceThreshold = percentDifferenceThreshold;
         }
 
         public virtual void Test(string name, ImageSnapshot actualImage)
         {
             string imageFileName = $"{name}{actualImage.Format.GetFileExtension()}";
 
-            string baselineImageFile = Path.Combine(this.snapshotsBaselineDirectory, imageFileName);
+            string baselineImagePath = Path.Combine(this.snapshotsBaselineDirectory, imageFileName);
 
-            if (!File.Exists(baselineImageFile))
+            if (!File.Exists(baselineImagePath))
             {
                 Directory.CreateDirectory(this.snapshotsDiffDirectory);
 
-                actualImage.Save(this.snapshotsDiffDirectory, imageFileName);
+                actualImage.Save(this.snapshotsDiffDirectory, name);
 
                 this.Fail(
                     $"Baseline snapshot not yet created: {Path.Combine(this.snapshotsBaselineDirectory, imageFileName)}\n" +
                     $"Ensure new snapshot is correct:    {Path.Combine(this.snapshotsDiffDirectory, imageFileName)}\n" +
-                    $"and if so, copy it to the snapshots-baseline directory");
+                    $"and if so, copy it to the snapshots-baseline directory." +
+                    $"\n" +
+                    $"Command: vdiff {this.snapshotsBaselineDirectory} {this.snapshotsDiffDirectory}\n");
 
                 return;
             }
 
-            ImageSnapshot baselineImage = new ImageSnapshot(baselineImageFile);
+            ImageSnapshot baselineImage = new ImageSnapshot(baselineImagePath);
 
-            double difference = this.visualComparer.Compare(baselineImage, actualImage);
-            if (difference < this.differenceThreshold)
+            double percentDifference = this.visualComparer.Compare(baselineImage, actualImage);
+            if (percentDifference > this.percentDifferenceThreshold)
             {
-                this.Fail($"Snapshot different than baseline: {imageFileName} ({difference}% difference)");
+                string formattedPercentDifference = string.Format("{0:0.00}", percentDifference);
+                this.Fail(
+                    $"Snapshot different than baseline: {imageFileName} ({formattedPercentDifference}% difference)\n" +
+                    $"If the correct baseline has changed (this isn't a a bug), then update the baseline image.\n" +
+                    $"\n" +
+                    $"Command: vdiff {this.snapshotsBaselineDirectory} {this.snapshotsDiffDirectory}\n");
+                return;
             }
         }
 
         public void Fail(string message)
         {
+            // For multiline messages, ensure they start on a new line to be better formatted in VS test explorer results
+            if (message.Contains('\n') && !message.StartsWith("\n"))
+            {
+                message = "\n" + message;
+            }
+
             throw new VisualTestFailedException(message);
         }
     }
