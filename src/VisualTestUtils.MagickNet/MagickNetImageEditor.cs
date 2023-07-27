c66bf387f62a31034e288e5b7721b871d095d4da
Bret Johnson (from Dev Box) <bretjohn@microsoft.com>
2023-07-27 00:58:57 -0400

    Add ImageEditor support, for cropping

---
+++ src/VisualTestUtils.MagickNet/MagickNetImageEditor.cs
@@ -0,0 +1,35 @@
+﻿using ImageMagick;
+
+namespace VisualTestUtils.MagickNet
+{
+    public class MagickNetImageEditor : IImageEditor
+    {
+        MagickImage magickImage;
+
+        public MagickNetImageEditor(ImageSnapshot imageSnapshot)
+        {
+            this.magickImage = new MagickImage(imageSnapshot.Data);
+        }
+
+        public void Crop(int x, int y, int width, int height)
+        {
+            this.magickImage.Crop(new MagickGeometry(x, y, width, height));
+            this.magickImage.RePage();
+        }
+
+        public (int width, int height) GetSize() =>
+            (this.magickImage.Width, this.magickImage.Height);
+
+        public ImageSnapshot GetUpdatedImage()
+        {
+            ImageSnapshotFormat format = this.magickImage.Format switch
+            {
+                MagickFormat.Png => ImageSnapshotFormat.PNG,
+                MagickFormat.Jpeg => ImageSnapshotFormat.JPEG,
+                _ => throw new NotSupportedException($"Unexpected image format: {this.magickImage.Format}")
+            };
+
+            return new ImageSnapshot(this.magickImage.ToByteArray(), format);
+        }
+    }
+}
